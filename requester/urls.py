0a9f95ae91792d4a9c307ce1bce9cd1fd093d184
KavishkaGunathilaka <gihanthakavishka@gmail.com>
2021-01-04 21:41:06 +0530

    test3

--- requester/urls.py
+++ requester/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     path('home/', views.homeView, name='requester-home'),#url to home page
-    path('home/', views.homeView, name='requester-home'),#url to home page
 ]
