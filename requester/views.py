527159932e713ce779b03b58b3e796634f08ab2f
KavishkaGunathilaka <gihanthakavishka@gmail.com>
2021-01-04 23:10:47 +0530

    NavBar

--- requester/views.py
+++ requester/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 #home page of requester app
+@login_required(login_url='')
 def homeView(request):
-    return render(request, 'requester/studenthome.html')#
+    if request.user.groups.filter(name='Lecturer').exists():
+        return render(request, 'requester/lecturerhome.html')#if user is a lecturer render lecturer home page
+    elif request.user.groups.filter(name='Student').exists():
+        return render(request, 'requester/studenthome.html')#if user is a lecturer render lecturer home page
 
