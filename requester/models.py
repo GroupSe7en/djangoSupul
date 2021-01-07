8faa350deed643a474b75148fa0823c0f84cf17e
KavishkaGunathilaka <gihanthakavishka@gmail.com>
2021-01-07 22:02:29 +0530

    Added StudentRequest model, Students and lecturers can view relevant requests on home page

--- requester/models.py
+++ requester/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+#model for request
+class StudentRequest(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, related_name="student_requests", on_delete=models.CASCADE)
+    reciever = models.ForeignKey(User, related_name="lecturer_requests", on_delete=models.SET_NULL, null=True)
+    accept_status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
--- requester/views.py
+++ requester/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from .models import StudentRequest
 
 #home page of requester app
 @login_required(login_url='')
 def homeView(request):
+    context = {
+        'StudentRequests': StudentRequest.objects.all()
+    }
+
     if request.user.groups.filter(name='Lecturer').exists():
-        return render(request, 'requester/lecturerhome.html')#if user is a lecturer render lecturer home page
+        return render(request, 'requester/lecturerhome.html', context)#if user is a lecturer render lecturer home page
     elif request.user.groups.filter(name='Student').exists():
-        return render(request, 'requester/studenthome.html')#if user is a lecturer render lecturer home page
+        return render(request, 'requester/studenthome.html', context)#if user is a lecturer render lecturer home page
 
